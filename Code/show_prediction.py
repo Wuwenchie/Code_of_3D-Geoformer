9fa7b42cab0c38dec048118cd1c7e2023c8d554f
Wuwenchie <161718523+Wuwenchie@users.noreply.github.com>
2025-05-13 14:34:21 +0800

    Update show_prediction.py

--- Code/show_prediction.py
+++ Code/show_prediction.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
+# data_in = xr.open_dataset('./model_3_level/Geoformer_1983_2021_output_3_level.nc')
+adr_oridata = "./data/GODAS_up150m_temp_nino_tauxy_kb.nc"
+data_in = xr.open_dataset(adr_oridata)
 
-data_in = xr.open_dataset('./model_3_level/Geoformer_1983_2021_output_3_level.nc')
 # 提取 sea_temperature 數據
 sea_temp = data_in['sea temperature']
 taux = data_in['taux']
@@ -26,6 +28,7 @@ lon = sea_temp['lon']
 # sea_temp = sea_temp.assign_coords(lon=lon_adjusted)
 # taux = taux.assign_coords(lon=lon_adjusted)
 # tauy = tauy.assign_coords(lon=lon_adjusted)
+
 # 緯度：-20° 到 20°，分段規則
 lat1 = np.arange(-20, -5, 1)  # -20° 到 -5°，步長 1°
 lat2 = np.arange(-5, 5.5, 0.5)  # -5° 到 5°，步長 0.5°
@@ -34,75 +37,77 @@ lat_new = np.concatenate([lat1, lat2, lat3])
 
 
 # 插值到新網格
-# sea_temp = sea_temp.interp(lon=lon, lat=lat_new, method='linear')
-# taux = taux.interp(lon=lon, lat=lat_new, method='linear')
-# tauy = tauy.interp(lon=lon, lat=lat_new, method='linear')
+sea_temp = sea_temp.interp(lon=lon, lat=lat_new, method='linear')
+taux = taux.interp(lon=lon, lat=lat_new, method='linear')
+tauy = tauy.interp(lon=lon, lat=lat_new, method='linear')
 print(sea_temp)
 
 fig, axes = plt.subplots(3, 4, figsize=(12, 4))
-# depth = 0   # for wind
-lat = 0     # for sea temperature
+depth = 0   # for wind
+# lat = 0     # for sea temperature
 times = ['2015-04-01', '2015-05-01', '2015-06-01', '2015-07-01', '2015-08-01', 
           '2015-09-01', '2015-10-01', '2015-11-01', '2015-12-01', '2016-01-01', 
           '2016-02-01', '2016-03-01'
         ]
+
+
 lead_time = 19
 
 for i, time in enumerate(times):
-    data = sea_temp.sel(time=time, lat=lat, method='nearest')[lead_time]  # lead_time=0
+    data = sea_temp.sel(time=time, depth=depth, method='nearest')[lead_time]  # lead_time=0
     taux_data = taux.sel(time=time, method='nearest')[lead_time]  # lead_time=0
     tauy_data = tauy.sel(time=time, method='nearest')[lead_time]  # lead_time=0
     print(data.shape)
 
-    # for sea temperature
-    lon, depth = np.meshgrid(data['lon'], data['depth'])
-    # 使用 2D 索引
-    row, col = divmod(i, 4)
-    contour = axes[row, col].contourf(lon, depth, data.values, cmap='RdBu_r', levels=20)
-    # ct1 = axes[row, col].contour(data.values, [-1.5, 0.0, 1.5, 3.0, 4.5], colors="k", linewidths=1)
-    # axes[row, col].clabel(contour, fmt='%1.1f', inline=True, fontsize=8, colors='black')
-
-    axes[row, col].invert_yaxis()
-    # axes[row, col].set_yticklabels(np.array[5, 20], fontsize=9)
-    axes[row, col].set_title(f'{time}')
-    axes[row, col].set_xlabel('Longitude (lon)')
-    # axes[row, col].set_xticks(lon[::5])  # 每 10 個點顯示一個標籤
-    # axes[row, col].set_xticklabels([f'{abs(l)}°E' for l in lon[::5]])
-    axes[row, col].set_ylabel('Depth (m)')
-
-    # tauxy
-    # Lon, Lat = np.meshgrid(data['lon'], data['lat'])
-
+    # # for sea temperature
+    # lon, depth = np.meshgrid(data['lon'], data['depth'])
     # # 使用 2D 索引
     # row, col = divmod(i, 4)
-    # contour = axes[row, col].contourf(Lon, Lat, data.values, cmap='RdBu_r', levels=20)
+    # contour = axes[row, col].contourf(lon, depth, data.values, cmap='RdBu_r', levels=20)
     # # ct1 = axes[row, col].contour(data.values, [-1.5, 0.0, 1.5, 3.0, 4.5], colors="k", linewidths=1)
     # # axes[row, col].clabel(contour, fmt='%1.1f', inline=True, fontsize=8, colors='black')
 
-    # # 繪製風應力向量（在最淺深度層，即 depth=5 米）
-    # # 創建用於風應力繪圖的網格（僅在最淺層）
-    # lon_quiver, lat_quiver = np.meshgrid(lon[::5], lat_new[::5])
-    # U = taux_data.values[::5, ::5]  # 經度方向風應力
-    # V = tauy_data.values[::5, ::5]  # 緯度方向風應力
-    # magnitude = np.sqrt(U**2 + V**2)
-
-    # # 檢查 quiver 數據形狀
-    # print("lon_quiver shape:", lon_quiver.shape)
-    # print("lat_quiver shape:", lat_quiver.shape)
-    # print("U shape:", U.shape)
-    # print("V shape:", V.shape)
-
-    # # 在圖表頂部（深度 5 米）繪製風應力向量
-    # scale = 100  # 調整箭頭大小
-    # axes[row, col].quiver(lon_quiver, lat_quiver, U, V, 
-    #                scale=scale, color='black', width=0.007)
-
+    # axes[row, col].invert_yaxis()
     # # axes[row, col].set_yticklabels(np.array[5, 20], fontsize=9)
     # axes[row, col].set_title(f'{time}')
-    # axes[row, col].set_xlabel('Longitude (°E)')
+    # axes[row, col].set_xlabel('Longitude (lon)')
     # # axes[row, col].set_xticks(lon[::5])  # 每 10 個點顯示一個標籤
     # # axes[row, col].set_xticklabels([f'{abs(l)}°E' for l in lon[::5]])
-    # axes[row, col].set_ylabel('Latitude')
+    # axes[row, col].set_ylabel('Depth (m)')
+
+    # tauxy
+    Lon, Lat = np.meshgrid(data['lon'], data['lat'])
+
+    # 使用 2D 索引
+    row, col = divmod(i, 4)
+    contour = axes[row, col].contourf(Lon, Lat, data.values, cmap='RdBu_r', levels=20)
+    # ct1 = axes[row, col].contour(data.values, [-1.5, 0.0, 1.5, 3.0, 4.5], colors="k", linewidths=1)
+    # axes[row, col].clabel(contour, fmt='%1.1f', inline=True, fontsize=8, colors='black')
+
+    # 繪製風應力向量（在最淺深度層，即 depth=5 米）
+    # 創建用於風應力繪圖的網格（僅在最淺層）
+    lon_quiver, lat_quiver = np.meshgrid(lon[::10], lat_new[::10])
+    U = taux_data.values[::10, ::10]  # 經度方向風應力
+    V = tauy_data.values[::10, ::10]  # 緯度方向風應力
+    magnitude = np.sqrt(U**2 + V**2)
+
+    # 檢查 quiver 數據形狀
+    print("lon_quiver shape:", lon_quiver.shape)
+    print("lat_quiver shape:", lat_quiver.shape)
+    print("U shape:", U.shape)
+    print("V shape:", V.shape)
+
+    # 在圖表頂部（深度 5 米）繪製風應力向量
+    scale = 1000000  # 調整箭頭大小
+    axes[row, col].quiver(lon_quiver, lat_quiver, U, V, 
+                   scale=scale, color='black', width=0.01)
+
+    # axes[row, col].set_yticklabels(np.array[5, 20], fontsize=9)
+    axes[row, col].set_title(f'{time}')
+    axes[row, col].set_xlabel('Longitude (°E)')
+    # axes[row, col].set_xticks(lon[::5])  # 每 10 個點顯示一個標籤
+    # axes[row, col].set_xticklabels([f'{abs(l)}°E' for l in lon[::5]])
+    axes[row, col].set_ylabel('Latitude')
 
     # 設置 X 軸標籤為經度格式 (E/W)
     # custom_ticks = np.linspace(120, -90, 5)  # 例如 -90, -60, -30, 0, 30, 60, 120
